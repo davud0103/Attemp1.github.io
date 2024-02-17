cd0db537db1ed34d2c57078391e28f330578de04
science2010 <157699973+davud0103@users.noreply.github.com>
2024-02-17 09:38:19 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var pObject = document.getElementById("pObject")
+document.querySelector(".a").addEventListener("click", function() {
+  pObject.innerHTML = heyvanContent;
+})
+
+document.querySelector(".b").addEventListener("click", function() {
+  pObject.innerHTML = bitkiContent;
+})
+
+document.querySelector(".c").addEventListener("click", function() {
+  pObject.innerHTML = gobelekContent;
+})
+
+document.querySelector(".d").addEventListener("click", function() {
+    pObject.innerHTML = bakteriyalarContent;
+})
+
+// Text-ler
+var heyvanContent = "Ənənəvi olaraq heyvanlar hazır üzvi birləşləmələrlə qidalalanlar və daima hərəkət edənlər olaraq iki qrupa ayrılır. Heyvanlar həm də xarici və daxili quruluşuna, çoxalma və inkişafına, davranışına görə müxtəlifdir. Bu müxtəlifliyi aydın təsəvvür etmək üçün fili, tısbağanı, köpəkbalığı və ağcaqanadı müqayisə etmək kifayətdir. Heyvanlar hər yerdə – Yer üzərində, torpaqda, suda, havada yaşamağa uyğunlaşmışdır. Bir çox heyvanlar bitkilərdə, digər heyvan orqanizmlərində, hətta insan orqanizmində yaşayır. Bütün heyvanlar yaşadıqları mühit şəraitinə uyğunlaşmışdır. Hava həyat tərzi (uçmaq həyatı) ilə əlaqədar quşlarda, yarasalarda, cücülərdə qanadlar inkişaf etmişdir, su həyatı ilə əlaqədar olaraq sürüşkən bədən forması, üzgəclər (balıqlarda) və ya kürəkşəkilli ətraf (balinada, suitidə) inkişaf etmişdir. Ona görə də heyvanın xarici quruluşuna gorə onun hansı mühitdə yaşamasını, necə hərəkət etməsini və qidasını necə tapmasını müəyyən etmək mümkündür.";
+
+var bitkiContent = "Botanika elminin tədqiqat obyekti olan bitkilər Yer kürəsində geniş yayılmışdır.Quru səthində hər il yaşıl bitkilər tərəfindən atmosferdən CO2 mənimsəməklə, günəş enerjisindən, torpaqdan daxil olan su və mineral birləşmələrdən istifadə etməklə 53 milyard ton biokütlə sintez edilir. Bu biokütlənin bir hissəsi kök və yerüstü qalıqlar şəklində təzədən torpağa qayıdır. Yaşıl bitkilər torpaqda üzvi maddələrin yeganə ilkin mənbəyidir. Onların torpaqəmələgətirici kimi əsas funksiyası maddələrin bioloji dövranı – torpaqdan qida elementlərinin və suyun mənimsənilməsi, üzvi kütlənin sintezi və həyat dövranı başa çatdıqdan sonra onun təzədən torpağa qaytarılmasıdır. Bioloji dövranın nəticəsi kimi – torpağın üst qatlarında potensial enerjinin və bitkilərin qida elementlərinin akkumulyasiyası torpaq profilinin tədrici inkişafını və torpağın əsas xassəsi olan münbitliyin inkişafını şərtləndirir.Yaşıl bitkilər torpaqdakı mineralların transformasiyasında – bəzilərinin parçalanmasında və yenilərinin sintezində, profilin kökyayılan hissəsinin strukturunun formalaşmasında, həmçinin su-hava və istilik rejiminin formalaşmasında iştirak edir. Yaşıl bitkilərin torpaqəmələgəlmədə iştirakının xüsusiyyəti bitkinin tipindən və bioloji dövranın intensivliyindən asılı olaraq müxtəlifdir.";
+
+var gobelekContent = "Göbələk (lat. Fungi) — mayalar və kiflər kimi mikroorqanizmlər və eləcə də geniş yayılmış papaqlı göbələklər kimi eukariot orqanizmlərin daxil olduğu geniş qrupdur. Göbələklər bitkilər, heyvanlar, bakteriyalar və protistlərdən fərqlənən və ayrıca bir aləm kimi Göbələklər adı altında birləşdirilən orqanizmlərdirlər. Xitindən təşkil olunan hüceyrə divarı göbələkləri bakteriyalar, bitkilər, heyvanlar və protistlərdən fərqləndirən başlıca əlamətdir(Protistlər və bitkilərdə hüceyrə divarı əsasən sellüozadan təşkil olunur). Əksər göbələklər mikotoksinlər adlanan bioaktiv birləşmələr əmələ gətirirlər. Göbələklər əsasən kosmopolitan orqanizmlərdirlər ki, onlara hər yerdə rast gəlmək mümkünür. Göbələkləri öyrənən elm mikologiya adlanır. Göbələklərin 100 000-ə qədər növü var."
+
+var bakteriyalarContent = "Bakteriyalar (lat. Bacteria) — təkhüceyrəli mikroorqanizmalardır. Prokaryotik mikroorqanizmlərin əksəriyyətini təşkil edən qrupdur. Böyüklükləri 0,1–10 mm arasındadır. Bakteriyaların dünya üzərində 3,5 milyard ildir yaşadıqları bilinməkdədir. Dünyada ən çox yayılmış orqanizmlərdir və yer kürəsində həyatın davam etməsi üçün böyük önəm təşkil etməkdədirlər. Arxebakteriyalar: əsasən, metansintezedici bakteriyalardır. Növlərinin sayı 40-dan çoxdur.Arxebakteriyalarda fotosintez xlorofil yox, bakteriorodopsin əsasında baş verir.Əsl bakteriyalar: anaerob və aerob heterotrofların, bəzən isə anaerob fotosintezedici avtotrof prokariotlaron, müxtəlif qruplarıdır.Oksifotobakteriyalar: aerob fotosintezedici prokariotlardır (sinobakteriyalar və xlorosibakteriyalar).Bakteriyaları kəşf edən Antoni van Levenhuk (XVII) olub. Ən qədim geoloji dövrdə biosferin yaranmasında ayrı-ayrı qrup prokariotların rolu böyükdür. bakterial fəallıq karbonatların, dəmir filizinin, sulfid, silisium, fosforit, boksit və s yataqlarının əmələ gəlməsinə səbəb olmuşdur."
